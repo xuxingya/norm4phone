d27bd7f762e2532b6093a913a0e9a7dc5dab403d
xingya.xu <xingya.xu@gmail.com>
2020-02-14 23:45:32 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='norm4phone',
+    version='1.0.0',
+    description='a tool to normalize different writing of phone numbers into standard format',
+    license='MIT License',
+    url='https://github.com/xuxingya/norm4phone',
+    author='xingya.xu',
+    author_email='xingya.xu@gmail.com',
+    packages=find_packages()
+)
